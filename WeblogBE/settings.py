c013ce6311a01feac93d6dad003e9feb0629ff58
BolisettiMeenakshi <janubolisetti392@gmail.com>
2025-08-19 21:24:38 +0530

    Modified: DEBUG = False and updated ALLOWED_HOSTS for Render

--- WeblogBE/settings.py
+++ WeblogBE/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-am_as&_)eth%ok&btgrr@035x&5n^fif304i)0$zwqm=nbgs-p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Weblog-BE.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
